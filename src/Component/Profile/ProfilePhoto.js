773a2e12c2cdd430872f2ec0d07cbb34c6e9d2fe
ines563 <sakkaines20@gmail.com>
2021-04-28 23:46:26 +0100

    first commit

---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            
+            <img src= "photo.jpg" width="500px" height="500px"  />
+        </div>
+    )
+}
+
+export default ProfilePhoto
